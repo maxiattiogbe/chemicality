4a023a48bf4c9ea4faa04e7763bd8def053a42c2
Maxi Attiogbe <maxiattiogbe@gmail.com>
2025-06-19 13:45:53 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100dvh",
+          background: "#0a0a0a",
+          color: "#ededed",
+          width: "100vw",
+        }}
+      >
+        <h1
+          style={{
+            fontWeight: "bold",
+            fontSize: "4rem",
+            color: "#4169e1",
+          }}
+        >
+          Chemicality
+        </h1>
+        <p style={{ fontSize: "1.5rem" }}>
+          Machine Learning for Molecular Discovery and Design
+        </p>
+        <div
+          style={{
+            fontSize: "1.1rem",
+            color: "#f5f5f5",
+            marginTop: "1rem",
+            maxWidth: "80vw",
+            textAlign: "center",
+          }}
+        >
+          <p>
+            Starting with small molecule drug design targeting tuberculosis
+          </p>
+          <p>
+            Aiming to expand to more pathogens and broader applications of molecular design
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
